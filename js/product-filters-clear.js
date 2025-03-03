1efce295b6d5a93466e076a289bd80a1acf6f539
George <gtsiokos@gmail.com>
2025-03-04 01:19:54 +0200

    initial

--- js/product-filters-clear.js
+++ js/product-filters-clear.js
@@ -11,15 +11,14 @@
 	filterItemOnClick = function( e ) {
 		var container = $( this ).closest( '.product-search-filter-terms' ),
 			origin_id = $( container ).attr( 'id' ),
-			//multiple  = $( container ).data( 'multiple' ),
 			clear     = $( this ).parent().data( 'term' ) === '',
+			clearTags = $( this ).data( 'term' ) === '',
 			taxonomy  = origin_id.indexOf( 'tag' ) >= 0 ? 'product_tag' : $( this ).parent().data( 'taxonomy' ),
 			term      = origin_id.indexOf( 'tag' ) >= 0 ? $( this ).data('term') : $( this ).parent().data( 'term' ),
 			term_name = $( this ).find( '.term-name' ).prop( 'innerHTML' );
 
 		// Create buttons
 		// @todo if clear is clicked remove those containers too
-		// @todo check brands
 		// @todo remove logs
 		if ( document.getElementById( 'wps-dynamic-clear-filters' ) !== null ) {
 			if ( !!term ) { 
@@ -30,13 +29,33 @@
 					console.log( 'Clear term exists' );
 					$( '#clear-filter-' + taxonomy + '-' + term ).remove();
 				}
-			} else {
-				if ( clear ) {
-					console.log( 'Clear' );
-					//var the_brand = 'product_brand';
-					//$("[id^='clear-filter-product_brand']").remove();
+			} else if ( clear ) {
+				console.log( 'Clear' );
+				var clear_option = $( this ).closest( '.nav-back' );
+				if ( $( this ).closest( '.nav-back' ) ) {
+					console.log( 'Closest nav-back' );
+					if ( $( clear_option ).parent().has( '[class^=product-search-]' ) ) {
+						var dirty_class = $( '[class^=product-search-]' ).prop( 'class' );
+						console.log( 'Found: ' + dirty_class );
+					}
+				} else {
+					console.log( 'Nothing' );
 				}
+				/*console.log( 'Clear_optoin: ' + clear_option );
+				if ( $( clear_option ).data( 'taxonomy' ) ) {
+					var clear_taxonomy = $( clear_option ).data( 'taxonomy' );
+					// product-search-pa_color-filter-item
+				} else if ( $( clear_option ).has( '[class^=product-search-]' ) ) {
+					console.log( 'Found ' );
+				}
+				console.log( 'Clear-taxonomy: ' + clear_taxonomy );*/
+				var the_brand = 'clear-filter-product_brand';
+				//$('[id^="'+the_brand+'"]').remove();
+			} else if ( clearTags ) {
+				console.log( 'Clear Tags' );
+				
 			}
+			
 		}
 
 		return false;
@@ -53,9 +72,6 @@
 			filter_item  = '',
 			current_taxonomy = '';
 
-		//console.log( 'taxonomy ' + taxonomy );
-		//console.log( 'term ' + current_term );
-		//console.log( filter_item );
 		if ( multiple && !clear ) {
 			var action = 'add';
 		} else {
@@ -97,7 +113,6 @@
 				$( '.product-filter-field' ).first().trigger( 'ixTermFilter', [ '', taxonomy, null, origin_id ] );
 			}
 			$( '#clear-filter-' + taxonomy + '-' + current_term ).remove();
-			//console.log( 'a.clear-filter-' + taxonomy + '-' + current_term );
 		}
 
 		return false;
