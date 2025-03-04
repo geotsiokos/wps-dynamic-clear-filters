53c0a11d3c81ff8445f388df1d011a9805791e8d
George <gtsiokos@gmail.com>
2025-03-04 12:07:06 +0200

    added css

--- js/product-filters-clear.js
+++ js/product-filters-clear.js
@@ -4,58 +4,59 @@
 		$( document ).on( 'click', '.product-search-filter-terms:not(.filter-dead) .product-search-product_brand-filter-item:not(.product-search-attribute-filter-item) a', filterItemOnClick );
 		$( document ).on( 'click', '.product-search-filter-terms:not(.filter-dead) .product-search-product_tag-filter-item', filterItemOnClick );
 		$( document ).on( 'click', '.product-search-filter-terms:not(.filter-dead) .product-search-attribute-filter-item:not(.product-search-product_cat-filter-item) a', filterItemOnClick );
-
-
 	});
 
 	filterItemOnClick = function( e ) {
-		var container = $( this ).closest( '.product-search-filter-terms' ),
-			origin_id = $( container ).attr( 'id' ),
-			clear     = $( this ).parent().data( 'term' ) === '',
-			clearTags = $( this ).data( 'term' ) === '',
-			taxonomy  = origin_id.indexOf( 'tag' ) >= 0 ? 'product_tag' : $( this ).parent().data( 'taxonomy' ),
-			term      = origin_id.indexOf( 'tag' ) >= 0 ? $( this ).data('term') : $( this ).parent().data( 'term' ),
-			term_name = $( this ).find( '.term-name' ).prop( 'innerHTML' );
+		var container  = $( this ).closest( '.product-search-filter-terms' ),
+			origin_id  = $( container ).attr( 'id' ),
+			clear      = $( this ).parent().data( 'term' ) === '',
+			clear_tags = $( this ).data( 'term' ) === '',
+			taxonomy   = origin_id.indexOf( 'tag' ) >= 0 ? 'product_tag' : $( this ).parent().data( 'taxonomy' ),
+			term       = origin_id.indexOf( 'tag' ) >= 0 ? $( this ).data('term') : $( this ).parent().data( 'term' ),
+			term_name  = $( this ).find( '.term-name' ).prop( 'innerHTML' );
 
 		// Create buttons
 		// @todo if clear is clicked remove those containers too
 		// @todo remove logs
 		if ( document.getElementById( 'wps-dynamic-clear-filters' ) !== null ) {
 			if ( !!term ) { 
-				if ( document.getElementById( 'clear-filter-' + taxonomy + '-' + term ) === null ) { console.log( taxonomy ); console.log( 'Clear-filter non exists');
-					$( '#wps-dynamic-clear-filters' ).append( '<a id="clear-filter-' + taxonomy + '-' + term + '">' + term_name + '</a>' );
+				if ( document.getElementById( 'clear-filter-' + taxonomy + '-' + term ) === null ) {
+					$( '#wps-dynamic-clear-filters' ).append( '<a class="clear-filter-term" id="clear-filter-' + taxonomy + '-' + term + '">' + term_name + '</a>' );
 					$( document ).on( 'click', '#clear-filter-' + taxonomy + '-' + term, { container: '#' + origin_id, taxonomy: taxonomy, term: term }, termClearOnClick );
 				} else {
-					console.log( 'Clear term exists' );
 					$( '#clear-filter-' + taxonomy + '-' + term ).remove();
 				}
 			} else if ( clear ) {
-				console.log( 'Clear' );
 				var clear_option = $( this ).closest( '.nav-back' );
-				if ( $( this ).closest( '.nav-back' ) ) {
-					console.log( 'Closest nav-back' );
-					if ( $( clear_option ).parent().has( '[class^=product-search-]' ) ) {
-						var dirty_class = $( '[class^=product-search-]' ).prop( 'class' );
-						console.log( 'Found: ' + dirty_class );
-					}
-				} else {
-					console.log( 'Nothing' );
-				}
-				/*console.log( 'Clear_optoin: ' + clear_option );
+				// Clear for attribute
 				if ( $( clear_option ).data( 'taxonomy' ) ) {
-					var clear_taxonomy = $( clear_option ).data( 'taxonomy' );
-					// product-search-pa_color-filter-item
-				} else if ( $( clear_option ).has( '[class^=product-search-]' ) ) {
-					console.log( 'Found ' );
+					var cleared_taxonomy = $( clear_option ).data( 'taxonomy' );
 				}
-				console.log( 'Clear-taxonomy: ' + clear_taxonomy );*/
-				var the_brand = 'clear-filter-product_brand';
-				//$('[id^="'+the_brand+'"]').remove();
-			} else if ( clearTags ) {
-				console.log( 'Clear Tags' );
-				
+				// Clear for product_cat, product_brand
+				if ( !cleared_taxonomy ) {
+					if ( $( this ).closest( '.nav-back' ) ) {
+						if ( $( clear_option ).has( '[class^=product-search-]' ) ) {
+							var dirty_class = $( clear_option ).prop( 'class' );
+							if ( dirty_class.indexOf( 'product_cat' ) >= 0 ) {
+								var cleared_taxonomy = 'product_cat';
+							} else {
+								if ( dirty_class.indexOf( 'product_brand' ) >= 0 ) {
+									var cleared_taxonomy = 'product_brand';
+								}
+							}
+						}
+					}
+				}
+				if ( cleared_taxonomy ) {
+					var clear_taxonomy_filters = 'clear-filter-' + cleared_taxonomy;
+					$('[id^="'+ clear_taxonomy_filters +'"]').remove();
+				}
+			} else if ( clear_tags ) {
+				// Clear for tag
+				var cleared_taxonomy = 'product_tag';
+				var clear_taxonomy_filters = 'clear-filter-' + cleared_taxonomy;
+				$('[id^="'+ clear_taxonomy_filters +'"]').remove();
 			}
-			
 		}
 
 		return false;
--- wps-dynamic-clear-filters.php
+++ wps-dynamic-clear-filters.php
@@ -32,6 +32,12 @@ class WPS_Dynamic_Clear_Filters {
 			array( 'jquery' ),
 			'1.0.0'
 		);
+		wp_enqueue_style(
+			'product-filters-clear-style',
+			plugins_url( 'wps-dynamic-clear-filters' ) . '/css/product-filters-clear-style.css',
+			array(),
+			'1.0.0'
+		);
 	}
 
 	public static function wps_dynamic_clear_filters( $atts ) {
